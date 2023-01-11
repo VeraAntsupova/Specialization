02ad0155753f6ca1925f71398dac293eec99eff7
Vera <vera.antsupova@yandex.ru>
2023-01-11 09:32:03 +0300

    Решение итоговой проверочной работы.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите слова через запятую:");
+
+string[] words = Console.ReadLine().Split(',');
+
+string[] results = new string[words.Length];
+for (int i = 0; i < words.Length; i++)
+{
+    if (words[i].Length <= 3) {
+        results[i] = words[i];
+    }
+    
+}
+
+
+foreach(var result in results)
+    System.Console.WriteLine(result);
